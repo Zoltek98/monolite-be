e2e77e432363c9abd502ac459ef39ccac37398f7
yuriboschetto <yuri.boschetto.external@enel.com>
2026-04-23 18:41:37 +0200

    rollback

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const pool = new Pool({
     user: process.env.DB_USER ,
     password: process.env.DB_PASS ,
     database: process.env.DB_NAME,
-    port: 6543,
+    port: 5432,
 });
 
 // --- MIDDLEWARE DI AUTENTICAZIONE ---
